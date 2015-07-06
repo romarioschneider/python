6d42ca42b60f984ad3e02a87b4695925d21fd6c6
romarioschneider <schneidersoft@ukr.net>
2015-07-07 00:14:52 +0300

    ex.1.3 initial

---
+++ M. Summerfield 'Programming in Python3'/ex.1.3.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3.4
+
+import random
+
+article = ("a", "the", "an")
+noun = ("boy", "girl", "cat", "dog", "man", "woman")
+verb = ("sang", "ran", "jumped", "shouted", "threw")
+adverb = ("loudly",)
