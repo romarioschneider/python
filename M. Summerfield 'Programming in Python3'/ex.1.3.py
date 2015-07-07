bab9763edeaf21dca1c751369f863d5533e8d848
romarioschneider <schneidersoft@ukr.net>
2015-07-07 23:16:27 +0300

    ex.1.3 done!

--- M. Summerfield 'Programming in Python3'/ex.1.3.py
+++ M. Summerfield 'Programming in Python3'/ex.1.3.py
@@ -5,4 +5,12 @@ import random
 article = ("a", "the", "an")
 noun = ("boy", "girl", "cat", "dog", "man", "woman")
 verb = ("sang", "ran", "jumped", "shouted", "threw")
-adverb = ("loudly",)
+adverb = ("loudly", "quietly", "well", "badly")
+
+iterable = "12345"
+for i in iterable:
+    line = ""
+    line = line + random.choice(article) + " " + random.choice(noun) + " " + random.choice(verb)
+    if random.randint(0, 1) == 1:
+        line = line + " " + random.choice(adverb)
+    print(line)
