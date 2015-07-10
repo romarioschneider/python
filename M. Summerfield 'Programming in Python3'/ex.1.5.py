196a5c2e21882e412153395e7ccb2ec614966348
romarioschneider <schneidersoft@ukr.net>
2015-07-11 00:23:16 +0300

    ex.1.5 done

--- M. Summerfield 'Programming in Python3'/ex.1.5.py
+++ M. Summerfield 'Programming in Python3'/ex.1.5.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3.4
 
+#as per conditions we must not use list.sort() and x % 2
+
 numbers_list = []
 sum = 0
 i = 0
@@ -42,7 +44,11 @@ try:
         sorted_list.append(min)
         numbers_list_l = numbers_list_l - 1
         
+    if ((len(sorted_list) / 2) != (len(sorted_list) // 2)):
+        median = sorted_list[len(sorted_list) // 2]
+    else:
+        median = (sorted_list[len(sorted_list) // 2] + sorted_list[len(sorted_list) // 2 - 1]) / 2
     print(sorted_list)   
-    print("count =", len(numbers_list), "sum =", sum, "lowest =", lowest, "higest =", higest, "mean = ", mean)
+    print("count =", len(sorted_list), "sum =", sum, "lowest =", lowest, "higest =", higest, "mean =", mean, "median =", median)
 except IndexError:
         print("Not received any number")
