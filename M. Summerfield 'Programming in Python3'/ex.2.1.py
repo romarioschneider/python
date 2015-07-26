9b4d6457cde95a99814f0aa63c4459da4245efbe
Roman Kholoshenko <schneidersoft@ukr.net>
2015-07-26 03:27:19 +0300

    Ex. 2.1 done

--- M. Summerfield 'Programming in Python3'/ex.2.1.py
+++ M. Summerfield 'Programming in Python3'/ex.2.1.py
@@ -3,26 +3,37 @@
 import sys
 import unicodedata
 
-def print_unicode_table(word):
+def print_unicode_table(words):
     print("decimal hex chr {0:^40}".format("name"))
     print("------- ----- --- {0:-<40}".format(""))
     
     code = ord(" ")
     end = sys.maxunicode
-    
+
     while code < end:
         c = chr(code)
         name = unicodedata.name(c, "*** unknown ***")
-        if word is None or word in name.lower():
+        for i in words:
+            if i in name.lower():
+                check_in = True
+            else:
+                check_in = False
+                break
+        if words is None or check_in == True:
             print("{0:7} {0:5X} {0:^3c} {1}".format(code, name.title()))
         code += 1
         
-word = None
+words = None
 if len(sys.argv) > 1:
     if sys.argv[1] in ("-h", "--help"):
         print("usage: {0} [string]".format(sys.argv[0]))
         word = 0
     else:
-        word = sys.argv[1].lower()
-    if word != 0:
-        print_unicode_table(word)
+        words = list()
+        i = 1
+        while i < len(sys.argv):
+            words.append(sys.argv[i].lower())
+            i += 1
+            
+    if words != 0:
+        print_unicode_table(words)
